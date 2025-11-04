572209deaf60ab2c60e756f0fbff1bc0785542f3
Solomon Teko <solomonpoghisio@gmail.com>
2025-11-04 23:06:53 +0300

    Add files via upload

---
+++ routes/PostRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import Post from "../models/post.js";
+
+const router = express.Router();
+
+// Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create a new post
+router.post("/", async (req, res) => {
+  try {
+    const newPost = new Post(req.body);
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export default router;
